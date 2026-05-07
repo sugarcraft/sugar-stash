d7747f1d0b45bf72cd4b1cbbca23dc291007517a
Joe Huss <detain@interserver.net>
2026-05-07 06:00:16 -0400

    Add Dutch locale for all libs (#259)

---
+++ lang/nl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Dutch translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git: starten mislukt',
+    'git.error'        => 'git: {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash: geen git-repo (.git niet gevonden in {cwd})',
+];
