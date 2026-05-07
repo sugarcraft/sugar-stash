d3e766fc7081942018d3078e0f9234c90bb12358
Joe Huss <detain@interserver.net>
2026-05-07 05:52:41 -0400

    Add Russian locale for all libs (#255)

---
+++ lang/ru.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Russian translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git: не удалось запустить',
+    'git.error'        => 'git: {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash: это не git-репозиторий (.git не найден в {cwd})',
+];
