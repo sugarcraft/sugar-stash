104f71385382d064d3b38b9b522cd729944cabc1
Joe Huss <detain@interserver.net>
2026-05-07 05:35:41 -0400

    Add French locale for all remaining libs (#247)

---
+++ lang/fr.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * French translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git : échec du lancement',
+    'git.error'        => 'git : {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash : ce n\'est pas un dépôt git (pas de .git dans {cwd})',
+];
