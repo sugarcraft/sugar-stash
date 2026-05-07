12205b647259bb6ee93291a8a05a27f6167867e0
Joe Huss <detain@interserver.net>
2026-05-07 05:58:30 -0400

    Add Polish locale for all libs (#258)

---
+++ lang/pl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Polish translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git: uruchomienie nie powiodło się',
+    'git.error'        => 'git: {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash: to nie jest repozytorium git (nie znaleziono .git w {cwd})',
+];
