2bd59ce6627744986674c42714c493ef13938c21
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Korean translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git: 시작 실패',
+    'git.error'        => 'git: {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash: git 저장소가 아닙니다 ({cwd}에서 .git을 찾을 수 없음)',
+];
