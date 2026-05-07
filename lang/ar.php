368f9e50dec05c6a6bfe01962167c0fabdc87c50
Joe Huss <detain@interserver.net>
2026-05-07 06:07:28 -0400

    Add Arabic locale for all libs (#262)

---
+++ lang/ar.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Arabic translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git: فشل البدء',
+    'git.error'        => 'git: {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash: ليس مستودع git (لم يتم العثور على .git في {cwd})',
+];
