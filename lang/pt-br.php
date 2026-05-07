cba499cdc829563fa77446ced41529caf19b7712
Joe Huss <detain@interserver.net>
2026-05-07 05:47:08 -0400

    Add Brazilian Portuguese locale for all libs (#252)

---
+++ lang/pt-br.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Brazilian Portuguese translations for sugar-stash.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'git.spawn_failed' => 'git: falha ao iniciar',
+    'git.error'        => 'git: {stderr}',
+    'cli.not_a_repo'   => 'sugar-stash: não é um repositório git (não foi encontrado .git em {cwd})',
+];
